6bc956c3fa1434be504a98c5ae538ac81cd02baa
Cruz Inc <cruzinc@Cruzs-MacBook-Pro-2.local>
2023-12-14 16:08:38 -0600

    Add title of page 'title-tag' dynamically on functions.php. Added urls with php-echo-site_url for About page. Clean page.php

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -6,8 +6,13 @@
         wp_enqueue_style('font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
         wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
         wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
-
     }
 
     add_action('wp_enqueue_scripts', 'university_files');
+    
+    function university_features() {
+        add_theme_support('title-tag');
+    }
+
+    add_action('after_setup_theme', 'university_features');
 ?>
