3f4f4aa2a70dbe10b058fc2b2f7ffe21f94a4222
Cruz Inc <cruzinc@Cruzs-MacBook-Pro-2.local>
2023-12-15 19:51:48 -0600

    Creation of child pages. Use of permalink with info of parent page to update sections in the page. Use of 'if' with php

--- app/public/wp-content/themes/fictional-university-theme/page.php
+++ app/public/wp-content/themes/fictional-university-theme/page.php
@@ -7,7 +7,7 @@
     while(have_posts()) {
         the_post(); ?>
         
-        <div class="page-banner">
+      <div class="page-banner">
       <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg') ?>)"></div>
       <div class="page-banner__content container container--narrow">
         <h1 class="page-banner__title"><?php the_title(); ?></h1>
@@ -18,12 +18,21 @@
     </div>
 
     <div class="container container--narrow page-section">
-      <div class="metabox metabox--position-up metabox--with-home-link">
-        <p>
-          <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span>
-        </p>
-      </div>
 
+      <?php 
+        // get the id of the parent given the current id of the page in question.
+        $theParent = wp_get_post_parent_id(get_the_ID()); 
+        if($theParent) {  ?>
+          <div class="metabox metabox--position-up metabox--with-home-link">
+            <p>
+              <a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent) ?>">
+                <i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent) ?></a> 
+                <span class="metabox__main"><?php the_title() ?></span>
+            </p>
+          </div>
+        <?php } 
+      ?>
+      
       <!-- 
       <div class="page-links">
         <h2 class="page-links__title"><a href="#">About Us</a></h2>
